34a8ac0e64c3336791a8e77e1963acbe40a80f0b
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-19 14:06:38 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default App
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div>{value}</div>
+      <div className="child">
+        <h1>Child Component</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
